2560050ce55af47802bd7a75a056cd72f4a194c6
aurelio-dasilva <aurelio-dasilva@outlook.com>
2023-05-18 08:49:06 -0300

    Ajuste na impressão do resultado

--- LendoEpulandoNomes/src/Problem.java
+++ LendoEpulandoNomes/src/Problem.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 public class Problem {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-// TODO: complete os espaços em branco com sua solução para o problema
+ //TODO: complete os espaços em branco com sua solução para o problema
 		String[] nomes = new String[10];
 		
 		for(int i=0 ; i<10 ; i++)
 			nomes[i] = sc.nextLine();
 
-			System.out.println(nomes[2+6+8]);
-			/*System.out.println(nomes[6]);
-			System.out.println(nomes[8]);*/
+			System.out.println();
+			System.out.println(nomes[2]);
+			System.out.println(nomes[6]);
+			System.out.println(nomes[8]);
+			sc.close();
 
 	}
 }
